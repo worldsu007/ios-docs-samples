629e10f13b61ede0fe763d0634043ff012469d75
Muxi Yan <muxi@users.noreply.github.com>
2019-09-30 16:26:23 -0700

    Fix Swift Speech-gRPC-Streaming (#127)
    * Fix an issue with swift bridging header that breaks the build
    
    * Update INSTALL-COCOAPODS file

--- speech/Swift/Speech-gRPC-Streaming/Speech/Speech-Bridging-Header.h
+++ speech/Swift/Speech-gRPC-Streaming/Speech/Speech-Bridging-Header.h
@@ -2,5 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <googleapis/CloudSpeech.pbobjc.h>
-#import <googleapis/CloudSpeech.pbrpc.h>
+#import <googleapis/google/cloud/speech/v1/CloudSpeech.pbobjc.h>
+#import <googleapis/google/cloud/speech/v1/CloudSpeech.pbrpc.h>
